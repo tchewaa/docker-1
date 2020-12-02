30b83c1718d7ea6cd6e1a7e16f3e6d0b06bbb469
Thabang <tchewa@student.wethinkcode.co.za>
2020-12-02 15:09:06 +0200

    added comments

--- 00_how_to_docker/03.sh
+++ 00_how_to_docker/03.sh
@@ -1,2 +1,3 @@
-# setting the docker machine environent variable 
+#setting the docker machine environent variable so you be able to 
+#interact with the VM on your local machine terminal 
 eval $(docker-machine env Char)
